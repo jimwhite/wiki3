c714ef241c9895e2ded018ef52b7c75eef78d2e2
Jim White <jim@ifcx.org>
2023-06-16 02:00:19 -0700

    Pick out the RFC label, title, and date from most RFC text files.

---
+++ indexer/rfc_title.py
@@ -0,0 +1,77 @@
+import os
+import re
+
+def parse_header(header_line):
+    pattern = re.compile(r'^(\x0c)?(RFC \d+[a-z]?)( -)?(  +)(.*)(  +)(.*)$')
+    match = pattern.match(header_line)
+
+    if match:
+        return match.group(2), match.group(5).rstrip(), match.group(7)
+
+    return None
+
+def parse_rfc_files(directory_path):
+    if not os.path.isdir(directory_path):
+        print(f"{directory_path} is not a valid directory.")
+        return
+
+    problematic_files = []
+    pattern_never_issued = re.compile(r'^rfc .* never issued', re.IGNORECASE)
+
+    for filename in os.listdir(directory_path):
+        if filename.endswith('.txt'):
+            try:
+                if not extract_header(directory_path, pattern_never_issued, filename):
+                    problematic_files.append(filename)
+            except Exception as e:
+                print(f"Error reading {filename}: {str(e)}")
+                problematic_files.append(filename)
+    
+    print("\nProblematic files: ", len(problematic_files))
+    for filename in problematic_files:
+        print(filename)
+
+def extract_header(directory_path, pattern_never_issued, filename):
+    with open(os.path.join(directory_path, filename), 'r') as file:
+        header_found = False
+        left_margin = float('inf')
+        right_margin = float('inf')
+        for line in file:
+            if line.startswith('\x0c'):  # End of page, break the loop
+                break
+                        # Find the widest common leading whitespace (left margin)
+            left_margin = min(left_margin, len(line) - len(line.lstrip()))
+                        # Find the widest common trailing whitespace (right margin)
+            right_margin = min(right_margin, len(line) - len(line.rstrip()))
+                    
+                    # Reset the file pointer
+        file.seek(0)
+        is_first_line = True
+        for line in file:
+            # Trim margins only if they contain only whitespace
+            line = line[left_margin:] if line[:left_margin].isspace() else line
+            line = line[:-right_margin] if right_margin > 0 and line[-right_margin:].isspace() else line
+
+
+            # If the line matches "RFC .... never issued ..."
+            if is_first_line:
+                is_first_line = False
+                if pattern_never_issued.match(line):
+                    print(f"{filename} contains a never issued RFC: {line.strip()}")
+                    header_found = True
+                    break
+                        
+            header = parse_header(line)
+            if header:
+                # If the title is missing, use the next line as the title
+                if not header[1]:
+                    header = (header[0], next(file, '').strip(), header[2])
+                print(f"File: {filename}, Header: {header}")
+                header_found = True
+                break
+
+        return header_found
+            
+
+# Use the function on a directory
+parse_rfc_files('/Users/jim/Projects/wiki3/ipfs/QmNvTjdqEPjZVWCvRWsFJA1vK7TTw1g9JP6we1WBJTRADM/rfc-data')
